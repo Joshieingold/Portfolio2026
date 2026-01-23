5dd2217aff2bb9092261825afa73244daaf06ca9
josh <joshlynch2001@gmail.com>
2026-01-23 02:52:21 -0400

    have highlights for active tab

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const contentIds = [
   "projects-content",
   "education-content",
 ];
+let currentHighlight = "";
+
 window.onload = function () {
   AddButtonListenerByName("#summary", SummaryClick);
-  AddButtonListenerByName("#header-title", TypeInTerminal("fastfetch"));
+  AddButtonListenerByName("#header-title", HeaderClick);
 };
+async function HeaderClick() {
+  await TypeInTerminal("fastfetch");
+  ClearMiniNav();
+  ClearNavHighlights();
+  // reset all the divs
+}
 
-function SummaryClick() {
-  CreateMiniNav();
-  TypeInTerminal("clear");
+async function SummaryClick() {
+  if (currentHighlight != "#summary") {
+    await TypeInTerminal("exec summary");
+    CreateMiniNav();
+    ChangeActiveHighlight("#summary");
+    currentHighlight = "#summary";
+  }
 }
 function AddButtonListenerByName(classOrId, func) {
   let targetElement = document.querySelector(classOrId);
@@ -21,15 +33,42 @@ function AddButtonListenerByName(classOrId, func) {
     targetElement.addEventListener("click", func);
   }
 }
+function ClearNavHighlights() {
+  allHighlights = document.querySelectorAll(".highlight-nav");
+  for (let i = 0; i < allHighlights.length; i++) {
+    allHighlights[i].classList.remove("highlight-nav");
+  }
+  currentHighlight = "";
+}
+function ChangeActiveHighlight(currentPage) {
+  let foundLink = document.querySelector(currentPage);
+  ClearNavHighlights();
+  if (foundLink) {
+    foundLink.classList.add("highlight-nav");
+    currentHighlight = currentPage;
+  }
+}
 function CreateMiniNav() {
-  let bigNav = document.querySelector(".navigation-container");
-  bigNav.style.height = "5%";
-  let navList = bigNav.querySelector(".nav-list");
+  let navElement = document.querySelector(".navigation-container");
+  navElement.style.height = "5%";
+  let navList = navElement.querySelector(".nav-list");
   navList.style.flexDirection = "row";
   navList.style.width = "100%";
   let itemList = navList.querySelectorAll("li");
   for (let i = 0; i < itemList.length; i++) {
     itemList[i].style.fontSize = "14pt";
+    itemList[i].style.width = "25%";
+  }
+}
+function ClearMiniNav() {
+  navElement = document.querySelector(".navigation-container");
+  navElement.style.height = "100%";
+  let navList = navElement.querySelector(".nav-list");
+  navList.style.flexDirection = "column";
+  navList.style.width = "100%";
+  let itemList = navList.querySelectorAll("li");
+  for (let i = 0; i < itemList.length; i++) {
+    itemList[i].style.fontSize = "24pt";
     itemList[i].style.width = "25%";
   }
 }
